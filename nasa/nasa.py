2490738edb6817ba309002bbadbb736982280392
sohanmithinti <63795028+sohanmithinti@users.noreply.github.com>
2021-03-10 17:41:32 +0200

    Add files via upload

---
+++ nasa/nasa.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+import requests
+
+start_url = "https://exoplanets.nasa.gov/discovery/exoplanet-catalog/"
+browser = webdriver.Chrome("/sohan_whjr/nasa/chromedriver")
+browser.get(start_url)
+time.sleep(10)
+header = ["name", "light_years", "planet_mass", "stellar_magnitude", "discovery_date", "hyperlink",
+ "plant_type", "planet_radius", "orbital_radius", "orbital_period", "eccentricity"]
+planet_data = []
+new_data = []
+
+def scrap():
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    for i in range(1, 3):
+        for ultag in soup.find_all("ul", attrs = {"class", "exoplanet"}): 
+            li_tags = ultag.find_all("li")
+            list1 = []
+            for index, litag in enumerate(li_tags):
+                if(index == 0):
+                    list1.append(litag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        list1.append(litag.contents[0])
+                    except:
+                        list1.append("")
+            hyperlink_li_tag = li_tags[0]
+            list1.append("https://exoplanets.nasa.gov"+hyperlink_li_tag.find_all("a", href = True)[0]["href"])
+            print(list1)            
+      
+            planet_data.append(list1)
+        browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click() 
+        
+   # with open("scraper.csv", "w") as f:
+        #csvwriter = csv.writer(f)
+        #csvwriter.writerow(header)
+        #csvwriter.writerows(planet_data) 
+
+scrap()    
+
+def scrape_more_data(hyperlink):
+    page = requests.get(hyperlink)
+    soup = BeautifulSoup(page.content, "html.parser")
+    for tr_tag in soup.find_all("tr", attrs = {"class":"fact_row"}):
+        td_tags = tr_tag.find_all("td")
+        list2 = []
+        for td_tag in td_tags:
+            try:
+                list2.append(td_tag.find_all("div", attrs = {"class":"value"})[0].contents[0])
+            except:
+                list2.append("")    
+        new_data.append(list2) 
